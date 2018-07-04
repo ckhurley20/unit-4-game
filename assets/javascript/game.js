9010405112ce11bd47c2c133563f8a19f75e6bc6
Cam Hurley <cameron.hurley20@gmail.com>
2018-07-04 12:29:48 -0400

    added win and loss functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,41 @@ var $redGem = document.getElementById("redGem");
 var $greenGem = document.getElementById("greenGem");
 var $blueGem = document.getElementById("blueGem");
 var $purpleGem = document. getElementById("purpleGem");
+var $wins = document. getElementById("wins");
+var $losses = document. getElementById("losses");
+var $currentScore = document. getElementById("currentScore");
+var $randomNumber = document. getElementById("randomNumber");
 
 var wins = 0;
 var losses = 0;
+var randomNumber = "";
+var currentScore = "",
+
+
+// GENERATE RANDOM NUMBER BETWEEN 19-120
+
+randomNumber = Math.floor(Math.random() * 121) + 19;
+
+$randomNumber.textContent = randomNumber;
+
+
+
+// CHECK FOR LOSS
+function checkLoss(){
+    if (currentScore > randomNumber) {
+        losses++;
+        $losses.textContent = losses;
+    }
+    checkWin();
+}
+
+
+
+//  CHECK FOR WIN
+function checkWin() {
+    if(currentScore = randomNumber) {
+        wins++;
+        $wins.textContent = wins;
+       
+    }
+}
