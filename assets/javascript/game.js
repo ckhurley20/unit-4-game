a5808154342614f292cf772288b706392ad6812f
Cam Hurley <cameron.hurley20@gmail.com>
2018-07-04 12:13:24 -0400

    added styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var $redGem = document.getElementById("redGem");
+var $greenGem = document.getElementById("greenGem");
+var $blueGem = document.getElementById("blueGem");
+var $purpleGem = document. getElementById("purpleGem");
+
+var wins = 0;
+var losses = 0;
